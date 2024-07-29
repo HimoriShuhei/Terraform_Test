355ab4dec2f6ecd601172de6fe5d3ce79618b95f
HimoriShuhei <85919680+HimoriShuhei@users.noreply.github.com>
2024-07-29 16:23:40 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 terraform {
-
   required_version = ">=0.12"
   
   required_providers {
@@ -14,96 +13,29 @@ provider "azurerm" {
   features {}
 }
 
+variable "resource_group_location" {
+  default = "ap-northeast-1"
+}
+
+locals {
+  common_tags = {
+    owner  = "terraformTest"
+    period = "20240831"
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "test-resources"
+  name     = "rg-terraformtest-resources"
   location = var.resource_group_location
+
+  tags = local.common_tags
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "testvnet"
+  name                = "terraformtestvnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-}
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "AzureFirewallSubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "pip" {
-  name                = "testpip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_firewall" "fw" {
-  name                = "testfirewall"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.subnet.id
-    public_ip_address_id = azurerm_public_ip.pip.id
-  }
-}
-
-resource "azurerm_firewall_application_rule_collection" "app-rc" {
-  name                = "apptestcollection"
-  azure_firewall_name = azurerm_firewall.fw.name
-  resource_group_name = azurerm_resource_group.rg.name
-  priority            = 100
-  action              = "Allow"
-
-  rule {
-    name = "testrule"
-
-    source_addresses = [
-      "10.0.0.0/16",
-    ]
-
-    target_fqdns = [
-      "*.google.com",
-    ]
-
-    protocol {
-      port = "443"
-      type = "Https"
-    }
-  }
-}
-
-resource "azurerm_firewall_network_rule_collection" "net-rc" {
-  name                = "nettestcollection"
-  azure_firewall_name = azurerm_firewall.fw.name
-  resource_group_name = azurerm_resource_group.rg.name
-  priority            = 100
-  action              = "Allow"
-
-  rule {
-    name = "dnsrule"
-
-    source_addresses = [
-      "10.0.0.0/16",
-    ]
-
-    destination_ports = [
-      "53",
-    ]
-
-    destination_addresses = [
-      "8.8.8.8",
-      "8.8.4.4",
-    ]
-
-    protocols = [
-      "TCP",
-      "UDP",
-    ]
-  }
+  tags = local.common_tags
 }
